654f51c649fa79b8b410984271c42a5a82061999
RomanDumas <dumasroman1011@gmail.com>
2025-03-30 18:52:01 +0300

    final-maybe

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,24 +5,24 @@ const Footer = function () {
     return (
         <div className='Footer'>
             <div className="Left-side">
-                <ul>Information</ul>
-                <ul>About Us</ul>
-                <ul>About Zip</ul>
-                <ul>Privacy Poulcy</ul>
-                <ul>Search</ul>
-                <ul>Teams</ul>
-                <ul>Orders and Returns</ul>
-                <ul>Contact Us</ul>
-                <ul>Advanced Search</ul>
+                <ul className="Up-Word">Information</ul>
+                <ul><a href="#">About Us</a></ul>
+                <ul><a href="#">About Zip</a></ul>
+                <ul><a href="#">Privacy Poulcy</a></ul>
+                <ul><a href="#">Search</a></ul>
+                <ul><a href="#">Teams</a></ul>
+                <ul><a href="#">Orders and Returns</a></ul>
+                <ul><a href="#">Contact Us</a></ul>
+                <ul><a href="#">Advanced Search</a></ul>
             </div>
             <div className="Right-side">
-                <ul>Adress</ul>
+                <ul className="Up-Word">Adress</ul>
                 <ul>Adress: 1234 Sreet Adress City Address, 1234</ul>
                 <ul>Phones:(00)1234 56789</ul>
                 <ul>We are open: Monday-Thursday: 9:00 AM - 5:30 PM</ul>
                 <ul>Friday: 9:00 AM - 6:00 PM</ul>
                 <ul>Saturday: 11:00 AM - 5:00 PM</ul>
-                <ul>E-mail: shop@email.com</ul>
+                <ul><a href="#">E-mail: shop@email.com</a></ul>
             </div>
         </div>
     )
