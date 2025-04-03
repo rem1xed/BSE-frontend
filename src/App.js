765026db51e8870df64bb264c30a25efcda7014d
Orest Mahotskyi <deregan2007@gmail.com>
2025-04-03 22:33:51 +0300

    qa! wq wq!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import Footer from './components/Footer';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <div className="page-conteiner">
-        <div className="Header-conteiner">
-          {/* here must be header */}
-        </div>
-        <div className="Content-conteiner">
-          {/* here must be content */}
-        </div>
-        <div className="Footer-conteiner">
-          <Footer />
-        </div>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
