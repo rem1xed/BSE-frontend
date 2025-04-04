474a76c0a245b8e76a485f2e9c993c83c24843e3
Orest Mahotskyi <deregan2007@gmail.com>
2025-04-04 11:46:53 +0300

    must fix h1 and button

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Login from './components/templates/login'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Login />
+      </div>
+    </div>
+  )
+}
+
+export default App
