bfb24c419d26033955efe91df49716cbb170c7ee
Orest Mahotskyi <deregan2007@gmail.com>
2025-04-13 21:26:09 +0300

    doneAddPage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import AddPage from './components/templates/addPage' // Шлях до вашого компонента
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddPage /> {/* Тут відображається ваш компонент */}
     </div>
-  );
+  )
 }
+
+export default App
