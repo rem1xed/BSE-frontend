7237a2fb6e4981dcaecab6e221bd78a24eb9fea7
RomanDumas <dumasroman1011@gmail.com>
2025-04-16 17:20:48 +0300

    added-thirt-viev-of-advertis,a-little-corrected-css,without-header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import AdvertismentListPage from './components/templates/AdvertismentListPage';
 // import Footer from './components/molecules/Footer';
 
 function App() {
   return (
     <div className="App">
+      <AdvertismentListPage />
         {/* here must be header */}
         {/* here must be main content */}
         {/* here must be footer */}
