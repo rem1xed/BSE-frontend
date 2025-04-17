adaf01a97bf4ca35f74e2dc45c36dfe8ca04becc
Romanjks123 <m7playyt@gmail.com>
2025-04-17 13:31:44 +0300

    last

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "./components/molecules/Header";
+import "./styles/Header.css";
 
 function App() {
   return (
     <div>
       <Header />
       <main style={{ margin: "30px" }}>
-        
-        
+        {/* Тут буде основний вміст */}
       </main>
     </div>
   );
