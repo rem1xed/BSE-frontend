f96ae13419bc0b25bd6a1d1f0d12ae4b5c6a662c
Orest Mahotskyi <deregan2007@gmail.com>
2025-04-28 21:58:40 +0300

    DoneForgetPassword

--- src/index.jsx
+++ src/index.jsx
@@ -6,19 +6,19 @@ import reportWebVitals from './reportWebVitals';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import NotFoundPage from './components/templates/NotFoundPage';
 import RegistrationPage from './components/templates/RegistrationPage';
-
+import ForgotPassword from './components/templates/forgetpassword'; // Імпортуйте компонент
 
 const router = createBrowserRouter([
   {path:'/', element: <App />},
   {path:'/register', element: <RegistrationPage />},
-  {path:'*', element: <NotFoundPage />},  //Fix issue with .css for every page
+  {path:'/forgotpassword', element: <ForgotPassword />}, // Додано новий маршрут
+  {path:'*', element: <NotFoundPage />},
 ]);
 
 createRoot(document.getElementById('root')).render(
-
   <React.StrictMode>
     <RouterProvider router={router}/>
   </React.StrictMode>
-)
+);
 
 reportWebVitals();
