de40c93ae29d6ec932e2e843dbd0ef51a216a298
Romanjks123 <m7playyt@gmail.com>
2025-05-06 23:01:24 +0300

    responsibility to footer

--- src/components/molecules/Footer.jsx
+++ src/components/molecules/Footer.jsx
@@ -4,27 +4,27 @@ import Button from '../atoms/Button';
 const Footer = function () {
     return (
         <footer >
-            <div className={style.side}>
-                <ul className={style.list}>
-                    <li className={style.list_element}><h2 className={style.footer_header}>Information</h2></li>
-                    <li className={style.list_element}><a className={style.link} href="#">About Us</a></li>
-                    <li className={style.list_element}><a className={style.link} href="#">About Zip</a></li>
-                    <li className={style.list_element}><a className={style.link} href="#">Privacy Policy</a></li>
-                    <li className={style.list_element}><a className={style.link} href="#">Search</a></li>
-                    <li className={style.list_element}><a className={style.link} href="#">Teams</a></li>
-                    <li className={style.list_element}><a className={style.link} href="#">Orders and Returns</a></li>
-                    <li className={style.list_element}><a className={style.link} href="#">Contact Us</a></li>
-                    <li className={style.list_element}><a className={style.link} href="#">Advanced Search</a></li>
+            <div className={style.side_left}>
+                    <h2 className={style.footer_header}>Information</h2>
+                <ul className={style.list_left}>
+                    <li className={style.list_element_left}><a className={style.link} href="#">About Us</a></li>
+                    <li className={style.list_element_left}><a className={style.link} href="#">About Zip</a></li>
+                    <li className={style.list_element_left}><a className={style.link} href="#">Privacy Policy</a></li>
+                    <li className={style.list_element_left}><a className={style.link} href="#">Search</a></li>
+                    <li className={style.list_element_left}><a className={style.link} href="#">Teams</a></li>
+                    <li className={style.list_element_left}><a className={style.link} href="#">Orders and Returns</a></li>
+                    <li className={style.list_element_left}><a className={style.link} href="#">Contact Us</a></li>
+                    <li className={style.list_element_left}><a className={style.link} href="#">Advanced Search</a></li>
                 </ul>
             </div>
             <div className={style.side}>
                 <ul className={style.list}>
                     <li className={style.list_element}>Address: 1234 Sreet Address City Address, 1234</li>
-                    <li className={style.list_element}>Phones:(00)1234 56789</li>
+                    <li className={style.list_element}>Phones:<span className={style.span_footer}>(00)1234 56789</span></li>
                     <li className={style.list_element}>We are open: Monday-Thursday: 9:00 AM - 5:30 PM</li>
                     <li className={style.list_element}>Friday: 9:00 AM - 6:00 PM</li>
                     <li className={style.list_element}>Saturday: 11:00 AM - 5:00 PM</li>
-                    <li className={style.list_element}><a className={style.link} href="#">E-mail: shop@email.com</a></li>
+                    <li className={style.list_element}><a className={style.link} href="#">E-mail: <span className={style.span_footer}>shop@email.com</span></a></li>
                 </ul>
             </div>
         </footer>
